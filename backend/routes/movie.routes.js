90c49201ba6e2cee3843a20065da63b5340293ae
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-01-24 20:47:23 +0530

    get and delete part done

--- backend/routes/movie.routes.js
+++ backend/routes/movie.routes.js
@@ -5,18 +5,56 @@ const movieRouter = express.Router()
 
 
 
-
-movieRouter.post("/add",  async (req, res) => {
+// post
+movieRouter.post("/add", async (req, res) => {
 
     try {
 
         const movie = new movieModel(req.body)
-         await movie.save()
-         res.status(200).send({ "msg":  movie})
-         console.log(movie)
+        await movie.save()
+        res.status(200).send({ "msg": movie })
+        console.log(movie)
 
     } catch (error) {
         res.status(400).send({ "error": err.message })
+    }
+
+})
+
+
+// get
+movieRouter.get("/", async (req, res) => {
+
+    try {
+
+        const movie = await movieModel.find()
+        res.status(200).send({ "msg": movie })
+
+    } catch (err) {
+        res.status(400).send({ "error": err.message })
+    }
+
+})
+
+
+
+
+
+// delete
+
+
+movieRouter.delete("/delete/:movieID", async (req, res) => {
+    const { movieID } = req.params
+
+    try {
+
+        await movieModel.findByIdAndDelete({ _id: movieID })
+        res.status(200).send({ "msg": `The movieID with ID:${movieID} has been deleted` })
+
+
+
+    } catch (err) {
+        res.status(400).send({ "error": message })
     }
 
 })
